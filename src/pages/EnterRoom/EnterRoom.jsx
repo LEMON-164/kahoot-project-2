8b78cd71854faf820922de1984f43130cd53b12f
tân lê <tan81ht2016@gmail.com>
2025-04-16 07:58:37 +0700

    test router

--- src/pages/EnterRoom/EnterRoom.jsx
+++ src/pages/EnterRoom/EnterRoom.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Input, Button, Layout, ConfigProvider } from 'antd';
+import { Link } from 'react-router-dom'; 
 import '../EnterRoom/EnterRoom.css';
 
 const { Content } = Layout;
@@ -10,21 +11,23 @@ const EnterroomPage = () => {
       <Layout className="kahoot-layout">
         <div className="centered-content">
           <h1 className="logo">QUIZZZZ!</h1>
-          
+
           <div className="kahoot-box">
             <Input
               placeholder="Game PIN"
               size="large"
               className="pin-input"
             />
-            <Button 
-              type="primary" 
-              size="large" 
-              block
-              className="enter-button"
-            >
-              Enter
-            </Button>
+            <Link to="/lobby">
+              <Button
+                type="primary"
+                size="large"
+                block
+                className="enter-button"
+              >
+                Enter
+              </Button>
+            </Link>
           </div>
         </div>
       </Layout>
